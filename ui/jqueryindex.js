605c6b75c7bb428f2cff8a973955616f30d650a5
AMEY PARUNDEKAR <AMEY PARUNDEKAR>
2016-12-02 16:57:00 +0530

    Added login page, Material Design, Dyanamic Pages, rm redundancies.

--- ui/jqueryindex.js
+++ ui/jqueryindex.js
@@ -1,5 +1,30 @@
+var minHeight = 30; // Define a minimum height for the middle div
+
+var resizeMiddle = function() {
+    var h = $('#text').height() - $('#header').height() - $('#footer').height();
+    h = h > minHeight ? h : minHeight;
+    $('#text').height(h);
+};
+
+
+
+
 
 $(document).ready(function(){
+
+resizeMiddle('#text');
+$(window).resize(resizeMiddle);
+
+$('#login').click(function(){
+//  $('#login').fadeTo('slow',0);
+
+  $('#login').fadeTo('slow',100);
+  $('#text').load('login.html');
+  document.getElementById('nav').innerHTML="<button id='login'> Back </button>";
+});
+
+
+
 
 	setInterval(function(){
 		$('blink').fadeIn('slow');
@@ -7,4 +32,7 @@ $(document).ready(function(){
 		,400);
 
 
-});
+
+
+
+ });
---
+++ ui/login.js
@@ -0,0 +1,13 @@
+var Id = document.getElementById('id');
+var id = nameInput.value;
+
+var submit = document.getElementById('submit_button');
+
+submit.onclick = function(){
+  //Make request
+
+  //Output
+
+
+  var ids = ['16blc1020','16blc1021','16blc1022'];
+}
