8395c3ef9be8fae32398d6fda8480566d40d02aa
AMEY PARUNDEKAR <AMEY PARUNDEKAR>
2016-12-08 00:15:10 +0530

    added_request_response

--- ui/login.js
+++ ui/login.js
@@ -1,13 +1,32 @@
-var Id = document.getElementById('id');
-var id = nameInput.value;
+var submit = $('#button');
 
-var submit = document.getElementById('submit_button');
-
-submit.onclick = function(){
+submit.click (function(){
   //Make request
+  console.log('click')
+  var request = new XMLHttpRequest();
+  //request.responseType = "text";
+  //Response;
+  request.onload = function(){
+    console.log('done1')
+    if(request.readystate = XMLHttpRequest.DONE){
+      console.log('done2');
+        if(request.status===200){
 
-  //Output
+          console.log(request.responseText);
+        document.getElementById('label').innerHTML = request.responseText;
+        $('#loginform').load('http://localhost:8082/ui/loginpsk.html');
+
+        }
+        else{
+          console.log("NOT DONE!")
+        }
+
+    }
+  }
 
 
-  var ids = ['16blc1020','16blc1021','16blc1022'];
-}
+  var id = document.getElementById('id').value;
+  console.log(id);
+  request.open('GET','http://localhost:8082/checkId/?id='+id,true);
+  request.send(null);
+});
