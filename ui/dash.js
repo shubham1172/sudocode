5e6900a2094b9018535bc7b632f0f8e5d1b5cc80
AMEY PARUNDEKAR <AMEY PARUNDEKAR>
2016-12-16 14:43:26 +0530

    UI_data_manipulation_done_implementation_remaining

--- ui/dash.js
+++ ui/dash.js
@@ -1,4 +1,7 @@
+var Handlebars = require('handlebars');
 $(document).ready(function(){
+
+  //Get Username
   var request_username = new XMLHttpRequest();
   request_username.onload = function(){
     console.log('done1')
@@ -26,6 +29,85 @@ $(document).ready(function(){
 $('#dropdown').click(function(){
   $('.dropdown-toggle').dropdown();
 });
+
+
+
+//Categories Request
+
+$('#categories').click(function(){
+
+  var request_categories = new XMLHttpRequest();
+  request_categories.onload = function(){
+
+      if(request_categories.readystate = XMLHttpRequest.DONE){
+
+        var data = request_categories.responseText;
+        if(request_categories.status===200||request_categories.status===304){
+          //mustache text 
+        }
+
+        else if(request_categories.status===500){
+
+          $('#dashbody').append('<div/>',{
+            id: 'error',
+            text: 'Error loading categories, try again later!'
+          });
+
+        }
+
+        else if(request_categories.status===403){
+          $('#dashbody').append('<div/>',{
+            id:'login_error',
+            text: 'Lol you didn\'t login'
+          });
+        }
+      }
+
+
+
+
+
+  }
+
+  request_categories.open('GET','http://localhost:8082/get-categories/',true);
+  request_categories.send(null);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
