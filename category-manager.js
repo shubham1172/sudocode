98d4e2b10d3e28fa1ec5e23d877535e67a28850a
Shubham Sharma <shubhamsharma1172@gmail.com>
2016-12-19 22:00:37 +0530

    Updated /get-category
    JSON modified

--- category-manager.js
+++ category-manager.js
@@ -15,7 +15,11 @@ exports.getCategory = function(req, res, pool){
       else{
         pool.any("SELECT name FROM sudocode.categories ORDER BY name ASC")
           .then(function(results){
-            res.status(200).send(results);
+            var response = {categories: []};
+            for(var x=0;x<results.length;x++){
+              response.categories.push({name: results[x].name});
+            }
+            res.status(200).send(JSON.stringify(response));
           })
           .catch(function(error){
             console.log(error.toString());
